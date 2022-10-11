a717f803e6264ea64743e6e50aed8de0dc6a6fe6
0RehanVerma0 <115353961+0RehanVerma0@users.noreply.github.com>
2022-10-11 17:44:57 +0530

    Add files via upload

---
+++ Trial.py
@@ -0,0 +1,17 @@
+from re import A
+
+
+print('Hello Everyone')
+
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+sum = a+b
+division = a/b
+multiplication = a*b
+difference = a-b
+Modulas = a%b
+print('sum of a,b',sum)
+print('division of a,b',division)
+print('multiplication of a,b',multiplication)
+print('difference of a,b',difference)
+print('Modulas of a,b',Modulas)
